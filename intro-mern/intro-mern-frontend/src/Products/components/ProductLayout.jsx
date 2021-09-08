916b39e357828a60c3068adadf7c7df8e6634a8c
Tomas Trinchero <trinchero52@gmail.com>
2021-09-08 20:49:44 -0300

    terceraClase

---
+++ intro-mern/intro-mern-frontend/src/Products/components/ProductLayout.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import Header from './Header'
+import AddButton from './AddButton'
+import Loading from './Loading'
+
+const ProductLayout = () => {
+    const [isLoading, setIsLoading] = useState(true)
+
+    return (
+      <>
+        <Header title="Products app" />
+        <AddButton />
+        {
+          isLoading
+          ? <Loading />
+          : 'mostrar resultado fetch'
+        }
+      </>  
+    )
+}
+
+export default ProductLayout
